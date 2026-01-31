e457918861cbf41cd38c8b7518b71d690adb0147
Akash <akash300383@gmail.com>
2026-01-31 20:39:10 +0530

    done

---
+++ FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/react-use-effect-examples/src/App.jsx
@@ -0,0 +1,24 @@
+import { use, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SimpleUseEffect from './SimpleUseEffect'
+
+function App() {
+let [flag,setFlag]=useState(true)
+let [msg,setMsg]=useState("Hide");
+  
+
+  return (
+    <div>
+      <h2>Toggle Component Visibility</h2>
+      <input type='button' value={msg} onClick={() => {
+        setFlag(!flag)
+        setMsg(flag ? "Show" : "Hide")
+      }}/>
+      {flag && <SimpleUseEffect />} 
+    </div>
+  )
+}
+
+export default App
---
+++ FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/react-use-effect-examples/src/SimpleUseEffect.jsx
@@ -0,0 +1,47 @@
+import {useEffect, useState } from "react"
+
+function SimpleUseEffect() {
+let [count,setCount] = useState(0)
+let [count1,setCount1] = useState(0)
+// simple useEffect without dependencies array
+// it call first and every time when component re-renders
+// useEffect(()=> {
+//     console.log("Component Mounted - loaded..")
+// })
+
+// useEffect with dependencies array ie empty array
+// it call only first time when component mounts
+// useEffect(()=> {
+//     console.log("Component Mounted - loaded..")
+// },[])
+
+// useEffect with dependencies array with specific state variable
+// it call only count not for count1
+// useEffect(()=> {
+//     console.log("Component Mounted - loaded..")
+// },[count])
+
+useEffect(()=> {
+    console.log("Component Mounted - loaded..SimpleUseEffect")
+
+    return ()=> {
+        console.log("Component Unmounted - cleanup..SimpleUseEffect")
+    }
+},[])
+    return(
+        <div>
+            <h2>useEffect Example</h2>
+            <p>Count: {count} and Count1 : {count1}</p>
+            <input type="button"
+            value="Increment Count"
+            onClick={() => setCount(count + 1)}
+            />
+            <input type="button"
+            value="Increment Count1"
+            onClick={() => setCount1(count1 + 1)}
+            />
+        </div>
+    )
+}
+
+export default SimpleUseEffect
