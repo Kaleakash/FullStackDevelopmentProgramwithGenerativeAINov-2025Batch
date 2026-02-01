49bf9a585b795ea5137da3ee68c340957eb4757e
Akash <akash300383@gmail.com>
2026-02-01 21:04:14 +0530

    done

--- FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/context-api-basic-example/src/Child1.jsx
+++ FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/context-api-basic-example/src/Child1.jsx
@@ -2,15 +2,16 @@ import { useContext } from "react";
 import Child4 from "./Child4";
 import MyContext from "./MyContext";
 
-function Child1() {
+function Child1(props) {
     // ready to consume data from context 
     // the child component can be any level. 
 let data = useContext(MyContext);
     return(
         <>
         <h2>Child1 Component</h2>
+        <p>Value in child1 using props as {props.fname}</p>
         <p>Value is in child1 component is {data.name} and age is {data.age}</p>
-        <Child4></Child4>
+        <Child4 fname={props.fname}></Child4>
         </>
     )
 }
