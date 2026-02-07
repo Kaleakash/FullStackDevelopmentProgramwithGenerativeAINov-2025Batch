609bb05c5ca62bcdb3c03c74f4321a3edd2a17a8
Akash <akash300383@gmail.com>
2026-02-07 22:03:55 +0530

    done

--- FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/product-crud-operation-using-axios/src/ProductOperation.jsx
+++ FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/product-crud-operation-using-axios/src/ProductOperation.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 function ProductOperation() {
-let [products,setProducts]=useState([]);
+let [products,setProducts]=useState([]);        // array of object 
+    let [product,setProduct]=useState({name:"",price:""}); //object name
 let [msg,setMessage]=useState("");
 let [errorMsg,setErrorMsg]=useState();
 let URL = "http://localhost:3000/products";
@@ -16,11 +17,40 @@ let loadAllProducts = async () => {
         setErrorMsg(error.message)
     }
 }
+let handleInputData= (event)=> {
+    let {name,value}=event.target;
+    //console.log(name+" "+value)
+    setProduct({...product,[name]:value});  // common function to set pname and price form property 
+}
+let storeProduct = async (event)=> {
+    event.preventDefault();
+    //console.log(product);
+    // logic to store the data in json form using axios 
+    try{
+        // post method to store the data
+        // 1st parameter url and 2nd parameter is json data. 
+    let result = await axios.post(URL,product)
+    console.log(result);
+    setMessage(result.statusText);
+
+    }catch(error){
+        console.log(error.message)
+    }
+    setProduct({name:"",price:""})
+}
 useEffect(()=> {
     loadAllProducts();
-},[])
+},[msg])
     return(
         <>
+        <h2>Add Product</h2>
+        <form onSubmit={storeProduct}>
+                <input type="text" name="name" value={product.name}
+                onChange={handleInputData} placeholder="Enter the Product Name"/><br/>
+            <input type="text" name="price" value={product.price}
+            onChange={handleInputData} placeholder="Enter the Product Price"/><br/>
+            <input type="submit" value="Add Product"/>
+        </form>
         <span style={{"color":"red"}}>{msg} {errorMsg}</span>
         <table border="1">
                 <thead>
