ed8a1c98848f167db85057fef0edcd08d82dc305
Akash <akash300383@gmail.com>
2026-02-08 21:39:17 +0530

    done

--- FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/react-redux-simple-app/src/reducer.js
+++ FSD MERN - Designing a Dynamic Front-end with React JAN 2026 batch 1/React JS Projects/react-redux-simple-app/src/reducer.js
@@ -12,11 +12,17 @@ function reducer(state=initialState,action){
         // 1st parameter state object with all property, 2nd property name which you want to change
         // third property value. 
         return {...state,n:state.n+1}
+        //return {...state,n:state.n+1,fname:"Vikash"}
     }
     if(action.type=="DECREMENT_N"){
         // 1st parameter state object with all property, 2nd property name which you want to change
         // third property value. 
         return {...state,n:state.n-1}
+    }
+    if(action.type=="CHANGE_FNAME"){
+        // 1st parameter state object with all property, 2nd property name which you want to change
+        // third property value. 
+        return {...state,fname:"Vikash"}
     }
     return state;
 }
