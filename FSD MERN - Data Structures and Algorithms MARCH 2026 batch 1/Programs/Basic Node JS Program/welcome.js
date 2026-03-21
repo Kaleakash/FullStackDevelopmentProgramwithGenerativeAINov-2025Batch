12ece8968153845c9189bf665a8e0708b7e2c16e
Akash <akash300383@gmail.com>
2026-03-21 20:19:43 +0530

    done

---
+++ FSD MERN - Data Structures and Algorithms MARCH 2026 batch 1/Programs/Basic Node JS Program/welcome.js
@@ -0,0 +1,5 @@
+console.log("Welcome to Node JS Example")
+function Hello() {
+    console.log("Welcome to JS function using node js")
+}
+Hello();
---
+++ FSD MERN - Data Structures and Algorithms MARCH 2026 batch 1/Programs/Primitive Data Structure/primitive_data_structure.js
@@ -0,0 +1,11 @@
+// create employee details using primitive data types 
+let id=100;
+let fname = "Ravi";
+let age = 21;
+let isActive = true;
+let msg;
+console.log("id is "+id+" Type of variable "+typeof(id))
+console.log("fname is "+fname+" Type of variable "+typeof(fname))
+console.log("age is "+age+" Type of variable "+typeof(age))
+console.log("isActive is "+isActive+" Type of variable "+typeof(isActive))
+console.log("message value is "+msg+" Type of variable "+typeof(msg))
