11d15f725e61f7c9965d762bd224b48758966483
Akash <akash300383@gmail.com>
2026-05-17 21:09:57 +0530

    done

---
+++ FSD MERN - Developing a Reliable Back-end with Node and Express MAY 2026 batch 1/Programs/http module examples/simpleServerApp.js
@@ -0,0 +1,12 @@
+let http = require("http");
+// http://localhost:3000    
+// http://127.0.0.1:3000
+// below code execute only if any client application send request through browser. 
+let server = http.createServer((request,response)=> {
+
+    response.end("Welcome to Http Node JS module");
+
+});
+
+// port number : range of port number 0 to 65535 
+server.listen(3000,()=>console.log("Server is up on port number 3000"))
