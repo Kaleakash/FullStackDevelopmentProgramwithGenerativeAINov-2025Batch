3137be4ce7a0de419d23266c825f9a115f9cbec4
Akash <akash300383@gmail.com>
2026-05-23 20:27:38 +0530

    done

--- FSD MERN - Developing a Reliable Back-end with Node and Express MAY 2026 batch 1/Programs/http-login-app/app.js
+++ FSD MERN - Developing a Reliable Back-end with Node and Express MAY 2026 batch 1/Programs/http-login-app/app.js
@@ -1,6 +1,7 @@
 let http = require("http");
 let url = require("url");
 let fs = require("fs");
+let queryString = require("querystring");
 const PORT = 3000;
 let server = http.createServer((request,response)=> {
 
@@ -10,14 +11,18 @@ let server = http.createServer((request,response)=> {
         let loginPage = fs.readFileSync("login.html");
         //console.log(loginPage.toString())
         response.write(loginPage);
+         response.end();
     }else if (urlRef.pathname=="/aboutUs") {
         let aboutUsPage = fs.readFileSync("aboutUs.html");
         response.write(aboutUsPage);
+         response.end();
     }else if (urlRef.pathname=="/contactUs") {
         let contactUsPage = fs.readFileSync("contactUs.html");
         response.write(contactUsPage);
+         response.end();
     }else if (urlRef.pathname=="/checkLogin") {
        
+        if(request.method=="GET"){
         let login = urlRef.query;
         let emailId = login.emailId;
         let password = login.password;
@@ -26,14 +31,36 @@ let server = http.createServer((request,response)=> {
         }else {
             response.write("Failure try once again")
         }
-        console.log(login)
+         response.end();
+        }else if(request.method=="POST"){
+            //response.write("Post Form Request")
+            let bodyData = "";
+            request.on("data",(formData)=> {
+                bodyData = bodyData+formData.toString();
+                //console.log(bodyData);
+            })
+
+            request.on("end",()=> {
+                console.log(bodyData);  // data consider as string 
+                let login = queryString.parse(bodyData);    // convert string format data in object format. 
+                let emailId = login.emailId;
+                let password = login.password;
+                if(emailId=="raj@gmail.com" && password=="raj@123"){
+                response.write("successfully login");
+                 response.end();
+                }else {
+                response.write("Failure try once again")
+                 response.end();
+                }
+            })
+
+        }
+       
+
     }else {
         let indexPage = fs.readFileSync("index.html");
         response.write(indexPage);
     }
-
-    response.end();
-
 });
 
 
