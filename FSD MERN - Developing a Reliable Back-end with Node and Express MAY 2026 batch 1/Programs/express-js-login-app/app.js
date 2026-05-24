7e6cd822f7aef7cbe9fe007971ab914aa7f0bf99
Akash <akash300383@gmail.com>
2026-05-24 19:49:13 +0530

    done

--- FSD MERN - Developing a Reliable Back-end with Node and Express MAY 2026 batch 1/Programs/express-js-login-app/app.js
+++ FSD MERN - Developing a Reliable Back-end with Node and Express MAY 2026 batch 1/Programs/express-js-login-app/app.js
@@ -1,6 +1,11 @@
 let express = require("express")
 let app = express();
 
+
+// middleware 
+// it use to extract form post data. 
+app.use(express.urlencoded({extended:true}));
+
 // http://localhost:5000
 
 app.get("/",(request,response)=> {
@@ -17,18 +22,28 @@ app.get("/",(request,response)=> {
 //     response.sendFile(__dirname+"/login.html")
 // })
 
-// http://localhost:5000/login
+// http://localhost:5000/login get method 
 
 app.get("/checkLogin",(request,response)=> {
    let emailId = request.query.emailId;
    let password = request.query.password;
    if(emailId=="admin@gmail.com" && password=="admin@123"){
-        response.send("Successfully login")
+        response.send("Successfully login with get method")
    }else {
-        response.send("Failure try once again")
+        response.send("Failure try once again with get method")
    }
 })
-
+// http://localhost:5000/login post method 
+app.post("/checkLogin",(request,response)=> {
+  let login = request.body;
+  //console.log(login);
+  //response.send("Post request")
+  if(login.emailId=="admin@gmail.com" && login.password=="admin@123"){
+        response.send("Successfully login with post method")
+   }else {
+        response.send("Failure try once again with post method")
+   }
+})
 app.get("/textFile",(request,response)=> {
     response.sendFile(__dirname+"/info.txt");
 })
